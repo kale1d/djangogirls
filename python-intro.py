0e002ec761daa9136df5ef7d795736d53a2fa1d9
kale1d <deneus@gmail.com>
2018-11-22 15:06:00 -0300

    first commit

---
+++ python-intro.py
@@ -0,0 +1,10 @@
+girls = ['Rachel', 'Monica', 'Phoebe']
+
+def hi(name):
+    print('Hi '+name+'!')
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+for value in range(1, 6):
+    print(value)
